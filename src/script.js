9930bb44e7471377319cc4656a25f348650dd741
nk-on <johndoe@example.com>
2024-10-20 20:00:22 +0400

    Feat:implementing feature which increases progress line then user clicks on next button

--- src/script.js
+++ src/script.js
@@ -1,3 +1,10 @@
 const lineElement = document.querySelector('[data-line]');
 const nextButton = document.querySelector('[data-next]');
 const prevButton = document.querySelector('[data-prev]');
+const progressBars = document.querySelectorAll('.progress-bar');
+nextButton.addEventListener('click',()=>{
+    const currWidth = lineElement.computedStyleMap().get('width').value;
+    console.log(currWidth)
+    if(currWidth+25 >= 110) return
+    lineElement.style.width = `${currWidth+25}%`;
+})
