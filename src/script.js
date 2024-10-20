fa1c76036ad344f123b2d0f2bd32825e6e9ce0a9
nk-on <johndoe@example.com>
2024-10-20 20:47:21 +0400

    Refactor:putting increase progress line function inside closure

--- src/script.js
+++ src/script.js
@@ -2,11 +2,17 @@ const lineElement = document.querySelector('[data-line]');
 const nextButton = document.querySelector('[data-next]');
 const prevButton = document.querySelector('[data-prev]');
 const progressBars = document.querySelectorAll('.progress-bar');
-let idx =0;
-nextButton.addEventListener('click',()=>{
+function increaseProgressLine() {
+  let idx = 0;
+  return () => {
     const currWidth = lineElement.computedStyleMap().get('width').value;
-    if(currWidth+25 >= 110) return;
+    if (currWidth + 25 >= 110) return;
     idx++;
     progressBars[idx].style.backgroundColor = '#05ffc3';
-    lineElement.style.width = `${currWidth+25}%`;
-})
+    lineElement.style.width = `${currWidth + 25}%`;
+  };
+}
+const increaseProgress = increaseProgressLine();
+nextButton.addEventListener('click', () => {
+  increaseProgress();
+});
