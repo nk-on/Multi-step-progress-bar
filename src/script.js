c9cdde1a59c495ae3cbc2a1fd9548e6141e49fe5
nk-on <johndoe@example.com>
2024-10-20 20:39:46 +0400

    Feat:coloring progress bars as user clicks on next button

--- src/script.js
+++ src/script.js
@@ -2,9 +2,11 @@ const lineElement = document.querySelector('[data-line]');
 const nextButton = document.querySelector('[data-next]');
 const prevButton = document.querySelector('[data-prev]');
 const progressBars = document.querySelectorAll('.progress-bar');
+let idx =0;
 nextButton.addEventListener('click',()=>{
     const currWidth = lineElement.computedStyleMap().get('width').value;
-    console.log(currWidth)
-    if(currWidth+25 >= 110) return
+    if(currWidth+25 >= 110) return;
+    idx++;
+    progressBars[idx].style.backgroundColor = '#05ffc3';
     lineElement.style.width = `${currWidth+25}%`;
 })
